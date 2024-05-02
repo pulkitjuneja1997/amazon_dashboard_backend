f609abeb7c49221caf479612c6f4ff8a31a1e24d
pulkitjuneja-1216 <pulkitjuneja@cedcommerce.com>
2024-05-03 00:10:39 +0530

    vercel home page mods

--- app.js
+++ app.js
@@ -16,7 +16,10 @@ const app = express();
 app.use(cors());
 
 
-routes.appRoutes(app);
+// routes.appRoutes(app);
+app.get( '/', function( req, res ){
+    res.status(200).json({'success': true, data: ['homepage']})
+})
 
 const PORT = process.env.PORT || 8000;
 
