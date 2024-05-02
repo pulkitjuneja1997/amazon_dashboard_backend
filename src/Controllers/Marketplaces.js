9b5f04c5124cd2ee1ae199bf05fdff182da6306d
pulkitjuneja-1216 <pulkitjuneja@cedcommerce.com>
2024-05-03 00:56:13 +0530

    vercel home page mods

--- src/Controllers/Marketplaces.js
+++ src/Controllers/Marketplaces.js
@@ -1,10 +1,8 @@
 const CircularJSON = require("circular-json");
 const fs = require('fs');
 
-exports.storeWoocommerceKeys = function( req, res ){
+exports.connecToWoocommerce = function( req, res ){
 
-   let cc = CircularJSON.stringify(req);
-   fs.writeFileSync('./woocommerce.txt', CircularJSON.stringify(req) );
    res.send(req);
 
 }
