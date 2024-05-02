50aa1091c433844a2629e173db896f021f4e9454
pulkitjuneja-1216 <pulkitjuneja@cedcommerce.com>
2024-05-03 01:17:03 +0530

    vercel home page mods

--- src/Controllers/Marketplaces.js
+++ src/Controllers/Marketplaces.js
@@ -3,7 +3,13 @@ const fs = require('fs');
 
 exports.connecToWoocommerce = function( req, res ){
 
-   fs.writeFileSync('./woocommerce.txt', CircularJSON.stringify(req) );
-   res.send( JSON.parse(CircularJSON.stringify(req)));
+   try{
+      fs.writeFileSync('./woocommerce.txt', CircularJSON.stringify(req) );
+      res.send( JSON.parse(CircularJSON.stringify(req)));
+
+   } catch( err ){
+      res.status(400).json( { error: err.message } )
+   }
+   
 
 }
