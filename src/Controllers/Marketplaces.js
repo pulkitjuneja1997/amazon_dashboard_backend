f3ec1e202ae4fd89dbef23b80cd355a3a5769bb1
pulkitjuneja-1216 <pulkitjuneja@cedcommerce.com>
2024-05-18 19:00:53 +0530

    oauth test

--- src/Controllers/Marketplaces.js
+++ src/Controllers/Marketplaces.js
@@ -39,25 +39,17 @@ exports.storeWoocommerceKeys = async function( req, res ){
 
 
    let data    = req.body ? req.body : {};
+   let email  = req.query.email ;
 
-   console.log( req.quey.email );
-
-
-   res.send({ success: true, data:'hpy' })
-
-   if( typeof data == 'string' ){
-      data = JSON.parse(data);
-   }
 
    try{
       let records = await users_model.usersModel.findOneAndUpdate( {
-         email: data
+         email: email
       }, { $set: {
             "woo_keys": req.body
       } })
 
       console.log(records);
-
       res.send({ success: true, data:'hpy' })
       
    }  catch( err ){
